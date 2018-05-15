397a106fcf50f1c70bfb96a46d89ce3d472ff5a2
Michael Blair <michael.41hertz@gmail.com>
2018-05-15 18:15:47 +1000

    Begin Angular implementation of personal page.

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(".dynamic-test").click(function() {
-    if ($(".content-pane").css('background-color') == 'rgb(0, 0, 0)') {
-        $(".content-pane").css('background-color', 'rgb(255,255,255)');
-    } else {
-        $(".content-pane").css('background-color', 'rgb(0, 0, 0)');
-    }
-    
-});
